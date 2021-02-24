ceba7db79621657b2998e41a18df29420a1fd72e
Jean Monteiro <jeanmontt@hotmail.com>
2021-02-24 18:01:52 +0000

    Added challenge box, contexts for communication between components and countdown/challenges functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
+import Head from 'next/head';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
-
-import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
-  return ( 
+  return (
     <div className={styles.container}>
       <Head>
         <title>Início | move.it</title>
@@ -24,7 +25,7 @@ export default function Home() {
         </div>
 
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
